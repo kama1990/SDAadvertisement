4184b835b3a2be0a3b30f41b7b54c388d0e11dec
kamila <czajkowska.kamila@wp.pl>
2023-06-20 10:44:15 +0200

    Created 1 page

--- advert/admin.py
+++ advert/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from advert.models import AdvertPost 
 # Register your models here.
+
+admin.site.register(AdvertPost)
--- advert/models.py
+++ advert/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class AdvertPost(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date = models.DateField()
+    category_choices = (('1', 'bardzo ważny'), ('2', 'ważny'), ('3', 'neutralny'))
+    category = models.CharField(max_length=15, choices=category_choices)
+
+    def __str__(self):
+        return self.title
