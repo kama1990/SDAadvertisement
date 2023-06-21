7b7b650348462d2dd0742ed5841ee785a1800e55
kamila <czajkowska.kamila@wp.pl>
2023-06-21 10:14:16 +0200

    added css - finished

--- advert/urls.py
+++ advert/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 # KROK 7
 # dodajemy ścięzkę detail , która będzie miał aodwołanie do views
@@ -12,3 +14,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('<int:postID>/', views.detail, name='detail')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
